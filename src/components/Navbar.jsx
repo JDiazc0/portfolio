54378eb072e02ec4866d2069de7f5edbc44e4b62
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2025-02-03 08:57:03 -0500

    navbar with styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,38 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Logo from "../assets/images/Logo.png";
 import { useLanguage } from "../hooks/useLanguage";
+import { useTheme } from "../hooks/useTheme";
 
 export default function Navbar() {
   const { t, setLang, currentLang } = useLanguage();
+  const { theme, toggleTheme } = useTheme();
   return (
     <>
-      <header>
-        <nav>
+      <header className="navbar">
+        <nav className="navbar__container">
           <div>
-            <img src={Logo} alt="ZeroDev" />
+            <a href="/">
+              <img src={Logo} alt="ZeroDev" className="navbar__logo" />
+            </a>
           </div>
-          <div>
-            <a href="aboutme">{t("navbar.aboutMe")}</a>
-            <a href="blog">{t("navbar.blog")}</a>
-            <a href="projects">{t("navbar.projects")}</a>
-            <a href="contact">{t("navbar.contact")}</a>
+          <div className="navbar__links">
+            <a href="aboutme" className="navbar__links--link">
+              {t("navbar.aboutMe")}
+            </a>
+            <a href="blog" className="navbar__links--link">
+              {t("navbar.blog")}
+            </a>
+            <a href="projects" className="navbar__links--link">
+              {t("navbar.projects")}
+            </a>
+            <a href="contact" className="navbar__links--link">
+              {t("navbar.contact")}
+            </a>
           </div>
-          <div>
-            <button>light mode</button>
+          <div className="navbar__settings">
+            <button onClick={toggleTheme}>
+              {theme === "light" ? "light" : "dark"}
+            </button>
             <button onClick={() => setLang(currentLang === "es" ? "en" : "es")}>
               {currentLang.toUpperCase()}
             </button>
